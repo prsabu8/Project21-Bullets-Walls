8259f5e193eb0acaf09e010c310cdc56e2fe4905
prsabu8 <68687182+prsabu8@users.noreply.github.com>
2020-07-31 18:37:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -41,3 +41,13 @@ function draw() {
   drawSprites();
 }
 
+function hasCollided(bullet, wall)
+{
+  bulletRightEdge = bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true;
+  }s
+  return false;
+}
